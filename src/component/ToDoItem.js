f31ce86100c6d4cce9200535e4b67de623ce8806
Abhishek4298 <abhishek.bhavsar@bacancytechnology.com>
2021-04-17 17:56:54 +0530

    mapping todos items using map.

--- src/component/ToDoItem.js
+++ src/component/ToDoItem.js
@@ -1,7 +1,7 @@
 const ToDoItem = ({ todo }) => {
 	return (<>
-		<h4>{todo.title}</h4>
-		<p>{todo.desc}</p>
+			<h4>{todo.title}</h4>
+			<h4>{todo.desc}</h4>
 	</>);
 }
 export default ToDoItem;
--- src/component/Todos.js
+++ src/component/Todos.js
@@ -1,11 +1,16 @@
 import ToDoItem from './ToDoItem';
 
 
-const Todos = ({todos}) => {
-	return ( <>
-	<h3> Todo's List </h3>
-	<ToDoItem todo={todos[0]}/>	
-	</> );
+const Todos = ({ todos }) => {
+	return (<div className="container">
+		<h1 className="text-center"> Todo's List </h1>
+		{		todos.map((todo) => {
+			return <ToDoItem todo={todo} />
+		})}
+
+		<button type="button" class="btn btn-danger btn-sm">Delete</button>
+
+	</div>);
 }
- 
+
 export default Todos;
