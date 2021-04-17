60cddad05476e1b703944ce394f13e4563cadb08
Abhishek4298 <abhishek.bhavsar@bacancytechnology.com>
2021-04-17 21:26:08 +0530

    Delete button works correctly and Footer Added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { useState } from 'react';
 import './App.css';
 // import UseStateLearn from "./component/UseStateLearn";
 // import Form from "./component/Form";
@@ -8,7 +9,7 @@ import Todos from './component/Todos';
 
 
 function App() {
-	let todos = [
+	let [todos, setTodos] = useState([
 		{
 			sno: "1",
 			title: "Hello title 1",
@@ -24,10 +25,15 @@ function App() {
 			title: "Hello title 3",
 			desc: "complete Mern"
 		}
-	]
+	]);
 
 	const onDelete = (todo) => {
-    console.log("I am ondelete of todo", todo);
+		console.log("I am ondelete of todo", todo);
+		setTodos(todos.filter((e) => {
+			return e !== todo;
+		}))
+		console.log("🚀 ~ file: App.js ~ line 33 ~ setTodos ~ setTodos", setTodos.legth)
+
 	}
 	return (
 		<>
--- src/component/Todos.js
+++ src/component/Todos.js
@@ -3,9 +3,10 @@ import ToDoItem from './ToDoItem';
 const Todos = ({ todos, onDelete }) => {
 	return (<div className="container">
 		<h1 className="text-center"> Todo's List </h1>
-		{todos.map((todo) => {
-			return <ToDoItem todo={todo} key={todo.sno} onDelete={onDelete}/>
-		})}
+
+		{todos.length ? todos.map((todo) => {
+			return <ToDoItem todo={todo} key={todo.sno} onDelete={onDelete} />
+		}) : <h2>No todos to display </h2>}
 	</div>);
 }
 
