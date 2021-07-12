081d7e95960cb6aa681b21e384914f54e0ce6fb3
Darshan <darshan.gauswami@bacancy.com>
2021-07-12 19:13:23 +0530

    for language translate.

--- src/component/FlowerAbout.js
+++ src/component/FlowerAbout.js
@@ -1,13 +1,42 @@
 import SingleProduct from './SingleProduct';
-import React, { useState } from 'react';
+import React, { useState, Suspense } from 'react';
 import PhoneIcon from '@material-ui/icons/Phone';
+import { useTranslation } from 'react-i18next';
+import i18next from 'i18next';
+
 const FlowerAbout = ({ about_page }) => {
+	const { t } = useTranslation();
+
+	function handleClick(lang) {
+		i18next.changeLanguage(lang)
+	}
+
 	const [contact, setContact] = useState({});
 	return (<>
+		<nav style={{ width: '100%', padding: '2rem 0', backgroundColor: 'gray' }}>
+			<button onClick={() => handleClick('en')} >
+				English
+			</button>
+			<button onClick={() => handleClick('ko')} >
+				Korean
+			</button>
+			<button onClick={() => handleClick('chi')} >
+				Chinese
+			</button>
+			<button onClick={() => handleClick('ger')} >
+				German
+			</button>
+			<button onClick={() => handleClick('hin')} >
+				Hindi
+			</button>
+		</nav>
 		<div className="about-section">
-			<h1>About Us Page</h1>
-			<p>Some text about who we are and what we do.</p>
-			<p>Resize the browser window to see that this page is responsive by the way.</p>
+			<h1>{t('Title.1')}</h1>
+			<p>{t('Description1.1')}</p>
+			<p>{t('Description1.2')}</p>
+			{/* <h1>About Us Page</h1>
+			<p>We are 3 team members.</p>
+			<p>We are providing full stack developement services in javascript.</p> */}
 		</div>
 
 		<h2 styles="text-align:center">GFC Team</h2>
