3041242f1c806f64cbeac0ffb8891fa99d7ca8fc
Live session user <abhishek.bhavsar@bacancy.com>
2021-07-14 12:39:30 +0000

    login backend api

--- server.js
+++ server.js
@@ -3,18 +3,31 @@ const app = express();
 const cors = require('cors');
 const router = require("./Backend/database/routes/index");
 const bodyParser = require('body-parser')
+const { Users } = require('../funcReact/Backend/database/models');
 
 const port = 8080;
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
- 
 
-app.use('/login', (req, res) => {
-  res.send({
-    token: 'test123'
-  });
+
+app.use('/login', async(req, res) => {
+  const {username,password}= req.body;
+  const userData = await Users.findAll({
+    where :{  
+      username,password
+    }
+  })
+  if(userData.length){
+    console.log("successfull login");
+    res.send({
+      token: 'test123'
+    });
+  }
+  else{
+    res.send("password not match");
+  }
 });
 app.use("/", router);
 
